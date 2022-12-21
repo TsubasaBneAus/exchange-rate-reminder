b8fb21bafa6ed814661102fe24cccdc4749a8a4d
Tsubasa Endo <tsubasa.bneaus@gmail.com>
2022-12-21 19:11:51 +1000

    add layout components

---
+++ components/Layout.tsx
@@ -0,0 +1,19 @@
+import Header from "./Header";
+import Footer from "./Footer";
+import { ReactNode } from "react";
+
+interface Props {
+  children: ReactNode;
+}
+
+const Layout = ({ children }: Props) => {
+  return (
+    <>
+      <Header />
+      <main>{children}</main>
+      <Footer />
+    </>
+  );
+};
+
+export default Layout;
