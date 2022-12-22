396506bae63be06bef8bd9abddde9aeaf79e365a
Tsubasa Endo <tsubasa.bneaus@gmail.com>
2022-12-22 12:55:54 +1000

    fix layouts

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,15 +3,21 @@ import styles from "../styles/Header.module.css";
 
 const Header = () => {
   return (
-    <header>
-      <Link href="/" className={styles.title}>Exchange Rate Reminder</Link>
+    <header className={styles.header}>
+      <Link href="/" className={styles.title}>
+        Exchange Rate Reminder
+      </Link>
       <nav>
         <ul>
-          <li>
-            <Link href="/signup" className={styles.navbar}>登録</Link>
+          <li className={styles.li}>
+            <Link href="/signup" className={styles.navbar}>
+              登録
+            </Link>
           </li>
-          <li>
-            <Link href="/login" className={styles.navbar}>ログイン</Link>
+          <li className={styles.li}>
+            <Link href="/login" className={styles.navbar}>
+              ログイン
+            </Link>
           </li>
         </ul>
       </nav>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,10 @@
+import styles from '../styles/Home.module.css'
+
 export default function Home() {
   return (
-    <div>
-      <h2>為替レートをメールでお知らせするアプリケーションです</h2>
-      <h2>
+    <div className={styles.layout}>
+      <h2 className={styles.explanation}>為替レートをメールでお知らせするアプリケーションです。</h2>
+      <h2 className={styles.explanation}>
         海外送金するタイミングやFXのために為替チャートを逐一確認するのが面倒な方へ！
       </h2>
     </div>
