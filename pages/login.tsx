5573765ac91ba6fb369f85fa28ae55f6886c7dca
Tsubasa Endo <tsubasa.bneaus@gmail.com>
2022-12-23 16:53:01 +0900

    add forms

--- pages/login.tsx
+++ pages/login.tsx
@@ -2,30 +2,38 @@ import { useState } from "react";
 import styles from "../styles/login.module.css";
 
 const Login = () => {
-  const [email, setEmail] = useState("example@gmail.com");
-  const [password, setPassword] = useState("password");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   return (
-    <form className={styles.form}>
+    <form className={styles.formContainer}>
       <h1 className={styles.title}>ログイン</h1>
       <hr className={styles.bar} />
-      <div className={styles.inputField}>
-        <label>メールアドレス</label>
-        <input
-          type="text"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
+      <div className={styles.form}>
+        <div className={styles.inputField}>
+          <label className={styles.inputLabel}>メールアドレス</label>
+          <input
+            className={styles.input}
+            type="text"
+            placeholder="example@gmail.com"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div className={styles.inputField}>
+          <label className={styles.inputLabel}>パスワード</label>
+          <input
+            className={styles.input}
+            type="text"
+            placeholder="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button className={styles.button} type="submit">
+          ログイン
+        </button>
       </div>
-      <div className={styles.inputField}>
-        <label>パスワード</label>
-        <input
-          type="text"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-      </div>
-      <button type="submit">ログイン</button>
     </form>
   );
 };
--- pages/register.tsx
+++ pages/register.tsx
@@ -1,5 +1,52 @@
+import { useState } from "react";
+import styles from "../styles/register.module.css";
+
 const Register = () => {
-  return <h1>登録</h1>;
+  const [email, setEmail] = useState("");
+  const [password1, setPassword1] = useState("");
+  const [password2, setPassword2] = useState("");
+
+  return (
+    <form className={styles.formContainer}>
+      <h1 className={styles.title}>アカウント登録</h1>
+      <hr className={styles.bar} />
+      <div className={styles.form}>
+        <div className={styles.inputField}>
+          <label className={styles.inputLabel}>メールアドレス</label>
+          <input
+            className={styles.input}
+            type="text"
+            placeholder="example@gmail.com"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div className={styles.inputField}>
+          <label className={styles.inputLabel}>パスワード</label>
+          <input
+            className={styles.input}
+            type="text"
+            placeholder="password"
+            value={password1}
+            onChange={(e) => setPassword1(e.target.value)}
+          />
+        </div>
+        <div className={styles.inputField}>
+          <label className={styles.inputLabel}>パスワード再入力</label>
+          <input
+            className={styles.input}
+            type="text"
+            placeholder="password"
+            value={password2}
+            onChange={(e) => setPassword2(e.target.value)}
+          />
+        </div>
+        <button className={styles.button} type="submit">
+          登録
+        </button>
+      </div>
+    </form>
+  );
 };
 
 export default Register;
