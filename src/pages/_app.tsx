27ebe87a9e18d8087d9686d84e10c280a8e382b6
Tsubasa Endo <tsubasa.bneaus@gmail.com>
2023-01-20 00:38:07 +0900

    add Head tag

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,10 +3,14 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Layout from '../components/Layout'
 import { SessionProvider } from "next-auth/react";
+import Head from "next/head";
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
     <SessionProvider session={pageProps.session}>
+      <Head>
+        <title>Exchange Rate Reminder</title>
+      </Head>
       <Layout>
         <Component {...pageProps} />
       </Layout>
