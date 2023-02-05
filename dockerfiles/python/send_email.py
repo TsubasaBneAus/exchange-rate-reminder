1d553d04699d5b7d2b486580af0f7ed3ebf3d593
Tsubasa Endo <tsubasa.bneaus@gmail.com>
2023-02-06 02:52:21 +0900

    modify the python code and docker file to run cron job

--- dockerfiles/python/send_email.py
+++ dockerfiles/python/send_email.py
@@ -1,6 +1,7 @@
 import os
 import requests
 from dotenv import load_dotenv
+import datetime
 
 # Load contents of the .env file
 load_dotenv()
@@ -8,3 +9,7 @@ load_dotenv()
 MYSQL_ROOT_PASSWORD = os.environ["MYSQL_ROOT_PASSWORD"]
 
 print(MYSQL_ROOT_PASSWORD)
+
+# test code
+now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+print(f"Current time: {now}")
