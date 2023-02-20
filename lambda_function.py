6e4c5caf0075036e4d2396fd1bc419ec15cb1dea
Tsubasa Endo <tsubasa.bneaus@gmail.com>
2023-02-20 09:49:25 +0900

    add lambda_handler function

--- lambda_function.py
+++ lambda_function.py
@@ -3,29 +3,30 @@ import requests
 from dotenv import load_dotenv
 import db
 
-# Load contents of the .env file
-load_dotenv()
+def lambda_handler(event, context):
+    # Load contents of the .env file
+    load_dotenv()
 
-# Configure the database to use
-host = os.environ["HOST"]
-port = os.environ['PORT']
-user = os.environ["USER"]
-password = os.environ["PASSWORD"]
-database = os.environ["DATABASE"]
-db_config = (host, port, user, password, database)
+    # Configure the database to use
+    host = os.environ["HOST"]
+    port = os.environ['PORT']
+    user = os.environ["USER"]
+    password = os.environ["PASSWORD"]
+    database = os.environ["DATABASE"]
+    db_config = (host, port, user, password, database)
 
-# Fetch the exchange rate data
-apikey = os.environ["API_KEY"]
-base_currency = "JPY"
-url = f"https://api.apilayer.com/exchangerates_data/latest?base={base_currency}"
-payload = {}
-headers = {"apikey": apikey}
+    # Fetch the exchange rate data
+    apikey = os.environ["API_KEY"]
+    base_currency = "JPY"
+    url = f"https://api.apilayer.com/exchangerates_data/latest?base={base_currency}"
+    payload = {}
+    headers = {"apikey": apikey}
 
-try:
-    response = requests.request("GET", url, headers=headers, data=payload)
-    response.raise_for_status()
-    json_data = response.json()
-except requests.HTTPError:
-    json_data = None
+    try:
+        response = requests.request("GET", url, headers=headers, data=payload)
+        response.raise_for_status()
+        json_data = response.json()
+    except requests.HTTPError:
+        json_data = None
 
-db.execute_query(db_config, json_data)
+    db.execute_query(db_config, json_data)
