880763998901459541dc11303e851ad1fb1cbea7
Tsubasa Endo <tsubasa.bneaus@gmail.com>
2023-03-03 07:23:04 +1000

    modify python file to fix HTTP Requests bug

--- dockerfiles/python/src-db/main.py
+++ dockerfiles/python/src-db/main.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from time import sleep
 from zoneinfo import ZoneInfo
 import os
 import requests
@@ -20,39 +21,47 @@ def main():
         database = os.environ["MYSQL_RDS_DATABASE"]
         db_config = (host, port, user, password, database)
 
-        # Fetch the exchange rate data
+        # Set up for fetching the exchange rate data
         apikey = os.environ["API_KEY"]
         base_currency = "JPY"
         url = f"https://api.apilayer.com/exchangerates_data/latest?base={base_currency}"
         payload = {}
         headers = {"apikey": apikey}
-        current_datetime = datetime.now(ZoneInfo("Asia/Tokyo")).strftime(
+        current_datetime1 = datetime.now(ZoneInfo("Asia/Tokyo")).strftime(
             "%Y-%m-%d %H:%M:%S %Z"
         )
 
-        # Handle exception of fetching the exchange rate data
-        try:
+        # Try to fetch the exchange rate data twice in case of not being able to fetch it
+        num_attempts = 2
+        for i in range(1, num_attempts):
+            current_datetime2 = datetime.now(ZoneInfo("Asia/Tokyo")).strftime(
+                "%Y-%m-%d %H:%M:%S %Z"
+            )
             response = requests.request("GET", url, headers=headers, data=payload)
-            response.raise_for_status()
-            json_data = response.json()
-            print(f"{current_datetime}: Data has been fetched decently!")
-        except requests.HTTPError:
-            # Retry to fetch data in case of not being able to fetch data at the 1st time
+
+            # Handle exception of fetching the exchange rate data
             try:
-                response = requests.request("GET", url, headers=headers, data=payload)
                 response.raise_for_status()
                 json_data = response.json()
-                print(f"{current_datetime}: Data has been fetched decently!")
+                print(f"{current_datetime2}: Data has been fetched decently!")
+                break
             except requests.HTTPError:
                 json_data = None
-                print(f"{current_datetime}: Failed to fetch the data!")
+                error_info = response.json()
+                if i == 2:
+                    print(f"{current_datetime2}: Failed to fetch the data!")
+                    print(f"{response.status_code}: {error_info['error']['info']}")
+                    break
+                else:
+                    sleep(120)
+                    continue
 
         # Execute a query to store the exchange rate data
         db.execute_query(db_config, json_data)
 
     except Exception:
         # Log that an error happened in main.py
-        print(f"{current_datetime}: An error happened in main.py!")
+        print(f"{current_datetime1}: An error happened in main.py!")
 
 
 # This block of code is only executed when the script is run directly
