6904930eb523dc1e153ecd0e72be131f9ab08621
Tsubasa Endo <tsubasa.bneaus@gmail.com>
2023-03-05 21:27:20 +1000

    modify Header.test.tsx to run and test decently

--- dockerfiles/nextjs/src/__test__/Header.test.tsx
+++ dockerfiles/nextjs/src/__test__/Header.test.tsx
@@ -31,46 +31,30 @@ jest.mock("next/router", () => ({
 }));
 
 jest.mock("next-auth/react");
-// jest.mock("next-auth/react", () => {
-//   const originalModule = jest.requireActual("next-auth/react");
-//   const mockSession = {
-//     expires: new Date(Date.now() + 2 * 86400).toISOString(),
-//     user: { username: "admin" },
-//   };
-//   return {
-//     __esModule: true,
-//     ...originalModule,
-//     useSession: jest.fn(() => {
-//       return { data: mockSession, status: "authenticated" };
-//     }),
-//   };
-// });
-// jest.mock("next-auth/react", () => {
-//   const originalModule = jest.requireActual("next-auth/react");
-//   return {
-//     __esModule: true,
-//     ...originalModule,
-//     useSession: jest.fn(() => {
-//       return { data: {}, status: "unauthenticated" };
-//     }),
-//   };
-// });
 
 describe("Header Component", () => {
-  test("renders 3 list items when there is session", async () => {
+  test("renders 3 list items when a user is authenticated", async () => {
+    (useSession as jest.Mock).mockReturnValue({
+      data: {
+        user: {
+          username: "test",
+        },
+      },
+      status: "authenticated",
+    });
+
     render(<Header />);
     expect(screen.getByText("Header.MyPage")).toBeInTheDocument();
     expect(screen.getByText("Header.Contact")).toBeInTheDocument();
     expect(screen.getByText("Header.SignOut")).toBeInTheDocument();
   });
-});
 
-describe("Header Component", () => {
-  test("renders 2 list items when there is no session", async () => {
+  test("renders 2 list items when a user is unauthenticated", async () => {
     (useSession as jest.Mock).mockReturnValueOnce({
-      data: {},
+      data: null,
       status: "unauthenticated",
     });
+
     render(<Header />);
     expect(screen.getByText("Header.SignIn")).toBeInTheDocument();
     expect(screen.getByText("Header.Contact")).toBeInTheDocument();
